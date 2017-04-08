8296594ffa8d23efd92da9aa0e8e4d6715c58cfa
MikySal78 <mikysal78@gmail.com>
2017-04-09 00:01:25 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# LEDE NNXX for LAMOBO-R1
+
+[![N|Solid](http://basilicata.ninux.org/images/Logo_Ninux_Basilicata_600-192.png)](http://basilicata.ninux.org)
+
+Compilazione di LEDE per HW AllWinner, esempio LAMOBO-R1
+
+# Clonare il repo con:
+```sh
+git clone https://github.com/NinuxBasilicata/LEDE-LAMOBO-R1.git
+```
+- editare il file compile.sh per settare le variabili
+- eseguire il file ./compile.sh
---
+++ compile.sh
@@ -0,0 +1,33 @@
+#/bin/bash -x
+compile=`pwd`
+version='custom/config-base'
+#version='custom/config-nodogspash'
+
+
+if [ -d source ]; then
+	cd source && git pull
+fi
+if [ ! -d source ]; then
+        git clone https://github.com/lede-project/source.git
+fi
+if [ -d Firmware ]; then
+        rm -rf Firmware
+fi
+if [ ! -d Firmware ]; then
+        mkdir Firmware
+fi
+
+cat $compile/feeds.conf > $compile/source/feeds.conf.default
+cat $compile/custom/etc/banner > $compile/source/package/base-files/files/etc/banner
+cp -R $compile/custom/etc/config $compile/source/package/base-files/files/etc/
+cp $compile/custom/etc/uci-defaults/* $compile/source/package/base-files/files/etc/uci-defaults/
+cat $compile/$version > $compile/source/.config 
+
+cd $compile/source
+./scripts/feeds update -a && ./scripts/feeds install -a && \
+exit 0
+make V=s -j 5 && \
+mv $compile/source/bin/targets/sunxi/generic/*.img.gz $compile/Firmware/ && \
+mv $compile/source/bin/targets/sunxi/generic/config.seed $compile/Firmware/
+
+make clean && make dirclean
